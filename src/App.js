d701171ee0ec96b1e90b1b44fa3d514298213152
Nele Taevere <ntaevere@gmail.com>
2021-01-25 13:28:39 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CoffeesView from "./components/CoffeesView";
+import NewCoffeeForm from "./components/NewCoffeeForm";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [coffees, setCoffees] = useState([]);
+  const updateCoffeeArray = (eachEntry) => {
+    setCoffees([...coffees, eachEntry]);
+  };
+  // const deleteCoffee = (id) => {
+  //   axios.get('ttp://localhost:5000/coffees', coffees)
+  //   .then(())
+  //   let newCoffees = [...coffees];
+  //   newCoffees = newCoffees.filter((coffee) => coffee._id != id);
+  //   setCoffees(newCoffees);
+  // };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/coffees", coffees)
+      .then((res) => setCoffees(res.data))
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h1>App</h1>
+      <CoffeesView coffees={coffees} />
+      <NewCoffeeForm
+        updateCoffeeArray={updateCoffeeArray}
+        // deleteCoffee={deleteCoffee}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CoffeesView.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { Row, Col, Table } from "reactstrap";
+
+const CoffeesView = (props) => {
+  const { coffees, deleteCoffee } = props;
+
+  const handleClick = (index) => {
+    deleteCoffee(index);
+    props.history.push("/coffees");
+  };
+
+  return (
+    <div className="mt-4">
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coffees.map((coffee, index) => (
+                <tr key={index}>
+                  <td>{coffee.name}</td>
+                  <td>{coffee.description}</td>
+                  <td>
+                    <button onClick={() => handleClick(coffee._id)}>x</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default CoffeesView;
