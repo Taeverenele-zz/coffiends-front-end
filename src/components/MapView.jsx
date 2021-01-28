f03faf24be95581bb3f5259bf7802783570c477b
Seb Brooks <sbrookstree@gmail.com>
2021-01-29 00:12:53 +1000

    fixed user order workflow & refactored a bit

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,88 +1,92 @@
-import "../App.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import {Motion, spring} from "react-motion";
-import {Container, Row} from "reactstrap";
-import OrderView from './OrderView'
+import { Motion, spring } from "react-motion";
+import { Container, Row}  from "reactstrap";
 import axios from "axios";
+import OrderView from './OrderView';
+import "../App.css";
 
 function MapView(props) {
-    const { coffee, userLocation, setCafe } = props;
-    const [show, setShow] = useState(false)
-    const [dropdownOpen, setOpen] = useState(false);
-    const toggle = () => setOpen(!dropdownOpen);
+  const { coffee, setCoffee, userLocation, setCafe } = props;
+  const [cafeData, setCafeData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [dropdownOpen, setOpen] = useState(false);
+  
+  const toggle = () => setOpen(!dropdownOpen);
 
-    const [cafeData, setCafeData] = useState([])
-
-    useEffect(() => {
-        const time = new Date
-        const postBody = {
-            location: userLocation,
-            time: (String(time.getHours()) + String(time.getMinutes())),
-            coffee: coffee.id
-        }
-        console.log(postBody)
-        axios
-        .post("http://localhost:5000/map/", postBody)
-        .then((res) => {
-            setCafeData(res.data);
-        })
-        .catch((error) => console.log(error.message));
-      }, []);
-
-    function handleClick(cafe) {
-        setCafe(cafe);
+  useEffect(() => {
+    const time = new Date();
+    const postBody = {
+      location: userLocation,
+      // time: (String(time.getHours()) + String(time.getMinutes())),
+      time: "1000", // uncomment line above to use actual time & comment this one out
+      coffee: coffee.id
     };
+    
+    axios
+      .post("http://localhost:5000/map/", postBody)
+      .then(res => setCafeData(res.data))
+      .catch(error => console.log(error.message));
+  }, []);
 
-    return (
-        <>
+  function handleClick(cafe, coffee) {
+    cafe.menu.map((menuitem) => {
+      if (menuitem.coffee == coffee.id) {
+        setCoffee(
+          {
+            id: coffee.id,
+            name: coffee.name,
+            price: menuitem.price
+          }
+        )
+      }
+    });
+    setCafe(cafe);
+    // setShow(true) // uncommment for popup ordering
+  };
 
-
-        <Motion defaultStyle={{x: 2000, opacity: 0}}
-         style={{x: spring(show ? 0 : 2000), opacity: spring(show ? 1 : 0)}}>
-
+  return (
+    <>
+      {/* <Motion
+        defaultStyle={{x: 2000, opacity: 0}}
+        style={{x: spring(show ? 0 : 2000), opacity: spring(show ? 1 : 0)}}
+      >
         {(style) => (
-            <Container>
-            <div className="Order-Panel" style={{transform: `translateX(${style.x}px)` , opacity: style.opacity ,}} >
-                    <OrderView coffee={coffee} setShow={setShow} >
-                    </OrderView>
+          <Container>
+            <div className="Order-Panel" style={{transform: `translateX(${style.x}px)` , opacity: style.opacity, }} >
+              <OrderView coffee={coffee} cafe={cafe} setShow={setShow} />
             </div>
-            </Container>
+          </Container>
         )}
-        </Motion>
-           
-        <MapContainer
-            center={userLocation}
-            zoom={16}
-            scrollWheelZoom={false}
-        >
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                
-            />
-                {cafeData.map((cafe) => (
-                    <Marker
-                        key={cafe._id}
-                        position={[cafe.location[0], cafe.location[1]]}
-                    >
-                        <Popup>
-                            <h2>{cafe.cafe_name}</h2>
-                            <p>{cafe.address}</p>
-                            <p>{coffee.name}</p>
-                            <p><b>${cafe.menu[0].price}</b></p>
-                            <Link onClick={() => setShow(true)}>BUY</Link>
-                            {/* to="/order" onClick={() => handleClick(cafe.name)} */}
-                        </Popup>
-                    </Marker>
-                ))}
+      </Motion> */}
+
+      <MapContainer center={userLocation} zoom={17} scrollWheelZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  
+        />
+
+        {cafeData.map((cafe) => (
+          <Marker
+            key={cafe._id}
+            position={[cafe.location[0], cafe.location[1]]}
+          >
+            <Popup key={cafe._id}>
+              <h3>{cafe.cafe_name}</h3>
+              <p>Hrs: {cafe.operating_hours[0]} - {cafe.operating_hours[1]}</p>
+              <p>{cafe.address}</p>
+              <p>{coffee.name}</p>
+              {cafe.menu.map((item) => 
+                item.coffee === coffee.id ? <Link to="/order" onClick={() => handleClick(cafe, coffee)}>${item.price.toFixed(2)} - BUY NOW</Link> : <></>
+              )}
+              {/* <Link onClick={() => setShow(true)}>BUY</Link> */}
+            </Popup>
+          </Marker>
+          ))}
         </MapContainer>
-
-      
-
-        </>
-    );
-}
+    </>
+  );
+};
 
 export default MapView;
