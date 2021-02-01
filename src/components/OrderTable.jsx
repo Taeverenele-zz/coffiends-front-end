0dab5310a9412d05a4d7fd323fc63ac0c791ba2e
Tye-Hadfield <tye_hadfield@hotmail.com>
2021-02-01 13:40:46 +1000

    Working on the Cafe Dashboard

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
-import { Row, Col, Table } from "reactstrap";
+import { Row,Table, Container, Button } from "reactstrap";
 import axios from "axios";
 
+
 const OrderTable = (props) => {
-  const { orders, getOrders, getPastOrders, loggedInUser } = props;
+  const { orders, getOrders, getPastOrders, loggedInUser, setOrders } = props;
 
   const completeOrder = async (id) => {
     const response = await axios.put(`http://localhost:5000/orders/${id}`);
-    const completedOrder = await response.data;
-    await getOrders();
-    await getPastOrders(false);
+    const completedOrder = response.data;
+    getOrders();
+    setOrders();
+    getPastOrders(false);
   };
 
   return (
     <>
-      <div className="mt-4">
+   <Container> 
+    <Row className="justify-content-center margin-add-top">
+    </Row>
         <Row>
-          <Col>
-            <Table hover>
+            <Table responsive>
               <thead>
                 <tr>
                   <th>Cafe</th>
@@ -30,13 +33,14 @@ const OrderTable = (props) => {
                   <th>Sugar</th>
                   <th>Pickup Time</th>
                   <th>Total</th>
+                  <th>COMPLETE</th>
                 </tr>
               </thead>
               <tbody>
                 {orders ? (orders.map((order) => (
                   <tr key={order._id}>
-                    <td>{order.cafe.cafe_name}</td>
-                    {/* <td>{order.user.user_name}</td> */}
+                    <th>{order.cafe.cafe_name}</th>
+                    <td>{order.user.user_name}</td>
                     <td>{new Date(order.order_date).toLocaleString("en-Au", {timeZone: "Australia/Brisbane"})}</td>
                     <td>{String(order.active)}</td>
                     <td>{order.coffee}</td>
@@ -47,7 +51,7 @@ const OrderTable = (props) => {
                     <td>${order.total.toFixed(2)}</td>
                     {(order.active && loggedInUser.role === "cafe") ? (
                       <td>
-                        <Link to="/dashboard" onClick={() => completeOrder(order._id)}>COMPLETE</Link>
+                         <Button onClick={() => completeOrder(order._id)}>Complete</Button>
                       </td>
                     ) : (<></>)
                     }
@@ -55,11 +59,113 @@ const OrderTable = (props) => {
                 ))) : <></>}
               </tbody>
             </Table>
-          </Col>
         </Row>
-      </div>
+    </Container>
     </>
   );
 };
 
 export default OrderTable;
+
+
+
+
+// <Row className="margin-add-top">
+// <Table responsive>
+// <thead>
+// <tr>
+// <th>Coffee</th>
+// <th>Milk</th>
+// <th>Sugar</th>
+// <th>Name</th>
+// <th>Phone</th>
+// <th>Time</th>
+// <th>Done</th>
+
+// </tr>
+// </thead>
+// <tbody>
+// <tr>
+//
+// </tr>
+// <tr>
+// <th scope="row">Long Black</th>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// </tr>
+// <tr>
+// <th scope="row">Espresso</th>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// </tr>
+// </tbody>
+// </Table>
+// </Row>
+
+
+// <Row className="justify-content-center margin-add-top">
+// <h1>Past Orders</h1>
+// <BsFillPlusSquareFill onClick={showOrders} className="align-self-center Dashboard-Margin-Left" / > 
+// </Row>
+
+// <Row className="Dashboard-Hide justify-content-center" id="Past-Orders">
+
+// <BsDashSquareFill className="Admin-Button-Margin justify-content-center" onClick={hideOrders} />
+
+// <Table responsive>
+// <thead>
+// <tr>
+// <th>Coffee</th>
+// <th>Milk</th>
+// <th>Sugar</th>
+// <th>Name</th>
+// <th>Phone</th>
+// <th>Time</th>
+// <th>Done</th>
+
+// </tr>
+// </thead>
+// <tbody>
+// <tr>
+// <th scope="row">Flat White</th>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// </tr>
+// <tr>
+// <th scope="row">Hot Chocolate</th>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// </tr>
+// <tr>
+// <th scope="row">Green Tea</th>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// <td>Table cell</td>
+// </tr>
+// </tbody>
+// </Table>
+// </Row>
+
+
+
+// )
+// };
